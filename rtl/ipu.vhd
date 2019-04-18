9958727d111ae2c330a672cb7712a0738209568a
Furkan Cayci <furkanca02@gmail.com>
2019-04-18 15:38:11 +0300

    added channel size to ipu

--- rtl/ipu.vhd
+++ rtl/ipu.vhd
@@ -9,6 +9,7 @@ use work.types.all;
 entity ipu is
 	generic(
 		PIXSIZE : integer := 8; -- pixel size
+		CHANNEL : integer := 3; -- number of color channels
 		H  : integer := 8; -- height of image
 		W  : integer := 10; -- width of image
 		KS : integer := 3    -- mask (kernel) size
@@ -17,19 +18,19 @@ entity ipu is
 		clk        : in  std_logic;
 		i_maskctrl : in  std_logic_vector(2 downto 0); -- mask control
 		i_active   : in  std_logic; --  input active signal
-		i_rgb      : in  std_logic_vector(23 downto 0); -- input rgb data
+		i_rgb      : in  std_logic_vector(CHANNEL*PIXSIZE-1 downto 0); -- input rgb data
 		o_active   : out std_logic; -- output active signal
-		o_rgb      : out std_logic_vector(23 downto 0)  -- output rgb data
+		o_rgb      : out std_logic_vector(CHANNEL*PIXSIZE-1 downto 0)  -- output rgb data
 	);
 end ipu;
 
 architecture rtl of ipu is
 	-- there are three active signals, but one is enough
 	-- leave two unconnected
-	signal active : std_logic_vector(2 downto 0) := (others => '0');
+	signal active : std_logic_vector(CHANNEL-1 downto 0) := (others => '0');
 	signal mask : mask_array(0 to KS**2-1);
-	signal s_i_rgb : signed(23 downto 0);
-	signal s_o_rgb : signed(23 downto 0);
+	signal s_i_rgb : signed(CHANNEL*PIXSIZE-1 downto 0);
+	signal s_o_rgb : signed(CHANNEL*PIXSIZE-1 downto 0);
 
 begin
 
@@ -42,13 +43,13 @@ begin
 	port map (i_ctrl=>i_maskctrl, o_mask=>mask);
 
 	-- generate workgroup(s) for r,g,b channels
-	gen_channels: for i in 0 to 2 generate
+	gen_channels: for i in 1 to CHANNEL generate
 	begin
 		ch: entity work.workgroup(rtl)
 		generic map (H=>H, W=>W, KS=>KS)
-		port map (clk=>clk, i_active=>i_active, i_pix=>s_i_rgb((i+1)*PIXSIZE-1 downto i*PIXSIZE),
-		i_mask=>mask, o_pix=>s_o_rgb((i+1)*PIXSIZE-1 downto i*PIXSIZE),
-		o_valid=>active(i));
+		port map (clk=>clk, i_active=>i_active, i_pix=>s_i_rgb(i*PIXSIZE-1 downto (i-1)*PIXSIZE),
+		i_mask=>mask, o_pix=>s_o_rgb(i*PIXSIZE-1 downto (i-1)*PIXSIZE),
+		o_valid=>active(i-1));
 	end generate;
 
 	-- since they are all parallel, one of them should be enough
